94e222087e7eff6c7bd1f07427d698c236cf7aa3
DataPashkov <66997327+DataPashkov@users.noreply.github.com>
2020-06-16 12:26:15 +0300

    Update getTasks.m

--- getTasks.m
+++ getTasks.m
@@ -112,6 +112,7 @@ in
 
 
     addColumnTypeOfElement = Table.AddColumn(expandGroupsName, "Тип элемента", each if [element_type] = "2" then "Сделка" else if [element_type] = "1" then "Контакт" else if [element_type] = "3" then "Компания" else if [element_type] = "12" then "Покупатель" else "Неизвестно" ),
+    addColumnTypeOfElement = Table.AddColumn(expandGroupsName, "Тип задачи", each Record.Field([[task_type]], "text")), 
     addColumnResultText = Table.AddColumn(addColumnTypeOfElement, "Result_Text", each Record.Field([result], "text")),
     replaceErrors = Table.ReplaceErrorValues(addColumnResultText, {{"Result_Text", null}}),
     delFinal = Table.RemoveColumns(replaceErrors,{"result"})
